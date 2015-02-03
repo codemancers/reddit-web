83280b8282009a811e5875797d8ae59986c99a2a
Amitava <amitava.bs@gmail.com>
2015-02-03 12:59:43 +0530

    Update angular-material to 0.7.1

--- src/scripts/jspm.js
+++ src/scripts/jspm.js
@@ -13,13 +13,12 @@ System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.11",
     "angular-markdown-directive": "github:btford/angular-markdown-directive@0.3.1",
-    "angular-material": "github:angular/bower-material@0.7.0",
+    "angular-material": "github:angular/bower-material@0.7.1",
     "angular-mocks": "github:angular/bower-angular-mocks@1.3.11",
     "angular-moment": "github:urish/angular-moment@0.9.0",
     "angular-sanitize": "github:angular/bower-angular-sanitize@1.3.11",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
     "css": "github:systemjs/plugin-css@0.1.0",
-    "hammer": "github:hammerjs/hammer.js@2.0.4",
     "lodash": "npm:lodash@3.0.0",
     "moment": "github:moment/moment@2.9.0",
     "showdown": "github:showdownjs/showdown@0.3.4",
@@ -38,12 +37,11 @@ System.config({
     "github:angular/bower-angular-sanitize@1.3.11": {
       "angular": "github:angular/bower-angular@1.3.11"
     },
-    "github:angular/bower-material@0.7.0": {
+    "github:angular/bower-material@0.7.1": {
       "angular": "github:angular/bower-angular@1.3.11",
       "angular-animate": "github:angular/bower-angular-animate@1.3.11",
       "angular-aria": "github:angular/bower-angular-aria@1.3.11",
-      "css": "github:systemjs/plugin-css@0.1.0",
-      "hammer": "github:hammerjs/hammer.js@2.0.4"
+      "css": "github:systemjs/plugin-css@0.1.0"
     },
     "github:jspm/nodelibs-process@0.1.0": {
       "process": "npm:process@0.10.0"
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// Ugly hack until angular-material removes Hammer.js dependency
-// https://github.com/angular/material/issues/456
-import Hammer from 'hammer';
-window.Hammer = Hammer;
-
 import angular from 'angular';
 import 'angular-material';
 import 'angular-ui-router';
